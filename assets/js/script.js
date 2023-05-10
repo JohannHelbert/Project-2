629624309139e94c660abdfd0c3a392376be58e7
JohannHelbert <>
2023-05-10 06:12:10 +0000

    Add user input code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 function checker(input) {
     var choices = ["rock", "paper", "scissor"];
     var num = Math.floor(Math.random() *3);
+    
     document.getElementById("comp_choice").
     innerHTML = 
-    ` Computer choose <span>${choices[num].
+    ` Computer choose <span> ${choices[num].
     toUpperCase()}</span>`;
+
+    document.getElementById("user_choice").
+    innerHTML =
+    ` You choose <span> ${input.toUpperCase()} </
+    span>`;
 }
